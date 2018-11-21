897cb9517d6b17e56b8d264839b840a9520855db
jmtgo <georgia.j.hills@durham.ac.uk>
2018-11-21 09:57:07 +0000

    now with comments

--- Hessian.py
+++ Hessian.py
@@ -1,3 +1,6 @@
+'''This module evaluates the Cpdf covariance matrix for any pdf (not NN type)'''
+
+
 import numpy as np
 import shelve
 import matplotlib.pyplot as plt
@@ -12,36 +15,37 @@ data_z3 = pd.read_csv('HEPData-ins1514251-v2-Table_21.csv')
 data_z4 = pd.read_csv('HEPData-ins1514251-v2-Table_24.csv')
 
 def si(jet, label,y,k):
-    return np.array(jet[label + str(k) + ''])[1:y+1]
+    #slices the data as there are more theoretical values than measured values and want to take the correct corresponding bins
+    return np.array(jet[label + str(k) + ''])[1:y+1] #for particular pdf parameter change k, is a vector length y
 
-def Up_Diff(jet,label, y, k):
+def Up_Diff(jet,label, y, k): #returns a vector length y.
     D = np.amax([np.zeros(y), (si(jet, label, y, k+1)- si(jet,label, y, 0)), (si(jet, label, y, k)- si(jet,label, y, 0))], axis=0)
-    return D
+    return D #finds the maximum of 0, si0-sik+, si0+sik-, where the k's come in pairs for plus and minus the parameter change. 
 
-def UpE_sq(jet, label, y):
+def UpE_sq(jet, label, y): #returns a vector
     UE = np.zeros(y)
     for k in range(1,53,2): #53 for CT10, 57 for CT14
         UE = (Up_Diff(jet, label, y, k))**2 + UE
-    return UE 
+    return UE #square all the D's for each k pair and adds them together.
 
-def Lo_Diff(jet,label, y, k):
+def Lo_Diff(jet,label, y, k): #returns a vector length y
     D = np.amin([np.zeros(y), (si(jet, label, y, k+1)+ si(jet,label, y, 0)), (si(jet, label, y, k)+ si(jet,label, y, 0))], axis=0)
-    return D
+    return D #finds the minimum of 0, si0+sik+, si0 + sik-
 
-def LoE_sq(jet, label, y):
+def LoE_sq(jet, label, y): #returns a vector length y
     LE = np.zeros(y)
     for k in range(1,53,2): #53 for CT10, 57 for CT14
         LE = (Lo_Diff(jet, label, y, k))**2 + LE
-    return LE 
+    return LE #square all the D's for each k pair and adds them together.
 
-def Av_e(jet, label, y):
+def Av_e(jet, label, y): #averages the upper and lower errors. 
     return (np.sqrt(LoE_sq(jet,label,y)) + np.sqrt(UpE_sq(jet, label, y)))/2
 
 def Cpdf(jet,label,y):
     Cpdf = np.zeros((y,y), dtype = np.float64)
     for i in range(y):
         for j in range(y):
-            Cpdf[i,j] = Av_e(jet,label,y)[i] * Av_e(jet,label,y)[j]
+            Cpdf[i,j] = Av_e(jet,label,y)[i] * Av_e(jet,label,y)[j] #outer product of the errors to find the Cpdf
     return Cpdf
 
 def Corr(jet,label,y):
